4d85d87ffc0f73ba98cf01fcc51dca6df75667cc
marpolll <minstarsoso@gmail.com>
2023-04-12 13:59:18 +0900

    Refactory

---
+++ Hello_01/src/Hello_01/Hello00.java
@@ -0,0 +1,12 @@
+package Hello_01;
+
+// This is class Block
+public class Hello00 {
+	public static void main(String[] args) {
+		System.out.println("Hello korea"); // 여기는 화면출력
+		System.out.println(30 + 40);
+		// DQ(큰 따옴표)로 묶인 데이터는 모두 문자열이다
+		System.out.println("30" + "40");
+		System.out.println(4587 + 2451);
+	}
+}
